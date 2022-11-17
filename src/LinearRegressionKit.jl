881d0c1824aaa2937aaaba316bfdda3758e9215f
Eric Quere <13007637+ericqu@users.noreply.github.com>
2022-11-17 21:14:39 +0100

    add infos

--- src/LinearRegressionKit.jl
+++ src/LinearRegressionKit.jl
@@ -181,7 +181,7 @@ function Base.show(io::IO, lr::linRegRes)
                 deepcopy(vec_stats_title), 
                 lr.updformula)
             if !isnothing(r_signif_codes)
-                @printf(io, "\tSignif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1")
+                @printf(io, "\n\tSignif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1")
             end    
         end
     end
